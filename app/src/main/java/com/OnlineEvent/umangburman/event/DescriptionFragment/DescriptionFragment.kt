19cd1423837ce2a8857f149810cbbad17a0d1e60
Huda Mohamed <huda.fci@gmail.com>
2020-04-06 13:16:38 -0700

    Add Schedule ui

---
+++ app/src/main/java/com/OnlineEvent/umangburman/event/DescriptionFragment/DescriptionFragment.kt
@@ -0,0 +1,79 @@
+package com.OnlineEvent.umangburman.event.DescriptionFragment
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
+import android.os.Bundle
+import android.text.Html
+import android.text.method.ScrollingMovementMethod
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.OnlineEvent.umangburman.event.EventFragment.EventViewModel
+import com.OnlineEvent.umangburman.event.MainActivity
+import com.OnlineEvent.umangburman.event.R
+import kotlinx.android.synthetic.main.description_fragment.*
+import kotlinx.android.synthetic.main.first_fragment.backButton
+import kotlinx.android.synthetic.main.schdule_fragment.*
+
+
+class DescriptionFragment : Fragment() {
+    private lateinit var root: View
+    private lateinit var learnMoreViewModel: LearnMoreViewModel
+    private lateinit var loginPreferences: SharedPreferences
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        (activity as MainActivity).setDrawerLocked(false)
+        (activity as MainActivity).showItem("second")
+        learnMoreViewModel = ViewModelProviders.of(this).get(LearnMoreViewModel::class.java)
+        root = inflater.inflate(R.layout.description_fragment, container, false)
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val eventId = arguments?.getInt("EventId")
+        setClickListeners()
+        eventId?.let { callEventsDescription(it) }
+    }
+
+    private fun setClickListeners() {
+        loginPreferences = activity!!.getSharedPreferences("loginPrefs", Context.MODE_PRIVATE)
+        backButton.setOnClickListener {
+            (activity as MainActivity).showDrwer(true)
+        }
+
+    }
+
+
+    private fun callEventsDescription(eventId: Int) {
+        eventDescriptionProgressBar.visibility = View.VISIBLE
+        val accessToken = loginPreferences.getString("accessToken", "")
+        if (accessToken != null) {
+            learnMoreViewModel.getEventDescriptionData(eventId, accessToken)
+        }
+        learnMoreViewModel.getData().observe(this, Observer {
+            eventDescriptionProgressBar.visibility = View.GONE
+            if (it != null) {
+                date.text = it.data.start_date
+                EventName.text = it.data.name
+                time.text = it.data.start_date + "  to  " + it.data.end_date
+                eventDescription.movementMethod = ScrollingMovementMethod()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    eventDescription.text = Html.fromHtml(it.data.description, Html.FROM_HTML_MODE_COMPACT)
+                } else {
+                    eventDescription.text = Html.fromHtml(it.data.description)
+                }
+            } else {
+                Toast.makeText(activity, "Network Error", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+
+}
